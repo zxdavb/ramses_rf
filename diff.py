c0eba3ddfddca90147e66b267149c76734083371
David Bonnes <zxdavb@bonnes.me>
2020-03-08 02:47:59 +0000

    add diff utility

---
+++ diff.py
@@ -0,0 +1,148 @@
+"""Evohome RF log diff utility."""
+import argparse
+import os
+import re
+from datetime import datetime as dt, timedelta
+
+DEBUG_ADDR = "172.27.0.138"
+DEBUG_PORT = 5679
+
+
+def _parse_args():
+    def extant_file(file_name):
+        """Check that value is the name of a existant file."""
+        if not os.path.exists(file_name):
+            raise argparse.ArgumentTypeError(f"{file_name} does not exist")
+        return file_name
+
+    def positive_int(value):
+        """Check that value is a positive int."""
+        ivalue = int(value)
+        if ivalue <= 0:
+            raise argparse.ArgumentTypeError(f"{value} is not a positive int value")
+        return ivalue
+
+    parser = argparse.ArgumentParser()
+
+    group = parser.add_argument_group(title="File names")
+    group.add_argument("file_one", type=extant_file)
+    group.add_argument("file_two", type=extant_file)
+
+    # Context control:
+    #   -B, --before-context=NUM  print NUM lines of leading context
+    #   -A, --after-context=NUM   print NUM lines of trailing context
+    #   -C, --context=NUM         print NUM lines of output context
+
+    group = parser.add_argument_group(title="Context control")
+    group.add_argument("-B", "--before", default=2, type=positive_int)
+    group.add_argument("-A", "--after", default=2, type=positive_int)
+
+    group = parser.add_argument_group(title="Debug options")
+    group.add_argument(
+        "-z",
+        "--debug_mode",
+        action="count",
+        default=0,
+        help="1=debug logging, 2=enabled, 3=wait for attach",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    """Main loop."""
+    args = _parse_args()
+
+    if args.debug_mode == 1:
+        # print(f"Debugging is enabled, additional logging enabled.")
+        pass
+
+    elif args.debug_mode > 1:
+        import ptvsd
+
+        print(f"Debugging is enabled, listening on: {DEBUG_ADDR}:{DEBUG_PORT}.")
+        ptvsd.enable_attach(address=(DEBUG_ADDR, DEBUG_PORT))
+
+        if args.debug_mode > 2:
+            print("Execution paused, waiting for debugger to attach...")
+            ptvsd.wait_for_attach()
+            print("Debugger is attached, continuing execution.")
+
+    compare(args)
+
+
+RSSI_REGEXP = re.compile(r"(-{3}|\d{3})")
+TIME_WINDOW = timedelta(seconds=1)
+
+
+def compare(config) -> None:
+    """Main loop."""
+
+    def parse(line: str) -> dict:
+        line = line.strip()
+        if RSSI_REGEXP.match(line[27:30]):  # is not a diagnostic line
+            return {"dt": line[:26], "rssi": line[27:30], "pkt": line[31:]}
+        return {"dt": line[:26], "rssi": None, "pkt": line[27:]}
+
+    def un_parse(pkt: dict) -> str:
+        if pkt["rssi"]:
+            return f"{pkt['dt']} {pkt['rssi']} {pkt['pkt']}"
+        return f"{pkt['dt']} {pkt['pkt']}"
+
+    def update_list(until):
+        if pkt2_list == []:
+            pkt2_list.append(parse(fh2.readline()))
+        while pkt2_list[-1]["dt"] and dt.fromisoformat(pkt2_list[-1]["dt"]) < until:
+            pkt2_list.append(parse(fh2.readline()))
+
+    def fifo_pkt(pkt_before: list, pkt: dict):
+        pkt_before.append(pkt)
+        if len(pkt_before) > config.before:
+            del pkt_before[0]
+
+    def print_before(pkt_before) -> list:
+        if len(pkt_before) == config.before:
+            print()
+        for pkt in pkt_before:
+            print(f"=== {un_parse(pkt)}")
+        return []
+
+    with open(config.file_one) as fh1, open(config.file_two) as fh2:
+        pkt1_before = []
+        pkt2_list = []
+        counter = 0
+
+        for line in fh1.readlines():
+            pkt1 = parse(line)
+            update_list(dt.fromisoformat(pkt1["dt"]) + TIME_WINDOW)
+
+            for idx, pkt2 in enumerate(pkt2_list):
+                matched = pkt1["pkt"] == pkt2["pkt"]
+
+                if matched:
+                    if idx > 0:  # some unmatched pkt2s
+                        counter = config.after
+                        pkt1_before = print_before(pkt1_before)
+
+                        for i in range(idx):
+                            pkt = pkt2_list[0]
+                            print(f">>> {un_parse(pkt)}")  # only in the 2nd file
+                            del pkt2_list[0]
+
+                    del pkt2_list[0]  # the matching packet
+                    break
+
+            if not matched:
+                counter = config.after
+                pkt1_before = print_before(pkt1_before)
+                print(f"<<< {un_parse(pkt1)}")  # only in the 1st file
+            else:
+                if counter > 0:
+                    counter -= 1
+                    print(f"=== {un_parse(pkt1)}")
+                else:
+                    fifo_pkt(pkt1_before, pkt1)
+
+
+if __name__ == "__main__":  # called from CLI?
+    main()
