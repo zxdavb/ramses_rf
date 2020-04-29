f34ba3d79b9e89ee80cce83f75ad5a5e7cba91ac
David Bonnes <zxdavb@bonnes.me>
2020-04-29 18:44:02 +0100

    switch to click from argparse

--- client.py
+++ client.py
@@ -4,10 +4,14 @@ import asyncio
 import os
 import sys
 
+import click
+
 from evohome import Gateway
 
 DEBUG_ADDR = "0.0.0.0"
 DEBUG_PORT = 5678
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
 def _parse_args():
@@ -129,39 +133,86 @@ def _parse_args():
     return args
 
 
-async def main(loop=None):
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.option("-k", "--known-devices", help="TBD", type=click.File("r"))
+@click.option("-w", "--device-whitelist", help="TBD", is_flag=True)
+@click.option("-d", "--database", help="TBD", type=click.Path("r"))
+@click.option("-m", "--message_log", help="TBD", type=click.Path("r"))
+@click.option("-r", "--raw-output", help="TBD", count=True)
+@click.option("-z", "--debug-mode", help="TBD", count=True)
+@click.pass_context
+# async def main(loop=None, **kwargs):
+def cli(ctx, **kwargs):
     # loop=asyncio.get_event_loop() causes: 'NoneType' object has no attribute 'serial'
-    """Main loop."""
-    args = _parse_args()
+    """A CLI for the evohome_rf library.
 
-    if args.debug_mode == 1:
+    evohome_rf is used to process RAMSES-II packets, either via RF or from a file.
+    """
+    ctx.obj = kwargs
+    return
+
+
+@click.command()
+@click.argument("input-file", type=click.File("r"), default=sys.stdin)
+@click.pass_obj
+def parse(obj, **kwargs):
+    """Parse a file for packets."""
+    print(f"parse: obj={obj}, kwargs={kwargs}")
+    main(**obj, **kwargs)
+
+
+@click.command()
+@click.argument("serial-port")
+@click.option("-p", "--probe-system", help="TBD", is_flag=True)
+@click.option("-e", "--execute-cmd", help="TBD", is_flag=True)
+@click.pass_obj
+def monitor(obj, **kwargs):
+    """Monitor a serial port for packets."""
+    print(f"monitor: obj={obj}, kwargs={kwargs}")
+    main(**obj, **kwargs)
+
+
+cli.add_command(monitor)
+cli.add_command(parse)
+
+
+def main(loop=None, **kwargs):
+    # loop=asyncio.get_event_loop() causes: 'NoneType' object has no attribute 'serial'
+    """A CLI for the evohome_rf library."""
+
+    # args = _parse_args()
+    # if args.debug_mode == 1:
+    if kwargs.get("debug_mode") == 1:
         print("Additional logging enabled (debugging not enabled).")
-        print(args)
+        # print(args)
         pass
 
-    elif args.debug_mode > 1:
+    # elif args.debug_mode > 1:
+    elif kwargs.get("debug_mode") > 1:
         import ptvsd
 
         print(f"Debugging is enabled, listening on: {DEBUG_ADDR}:{DEBUG_PORT}.")
         ptvsd.enable_attach(address=(DEBUG_ADDR, DEBUG_PORT))
 
-        if args.debug_mode > 2:
+        # if args.debug_mode > 2:
+        if kwargs.get("debug_mode") > 2:
             print("Execution paused, waiting for debugger to attach...")
             ptvsd.wait_for_attach()
             print("Debugger is attached, continuing execution.")
 
-    gateway = Gateway(**vars(args), loop=loop)
-
-    await gateway.start()
-
-
-if __name__ == "__main__":
     if sys.platform == "win32":  # better than os.name
         # ERROR:asyncio:Cancelling an overlapped future failed
         # future: ... cb=[BaseProactorEventLoop._loop_self_reading()]
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
+    # gateway = Gateway(**vars(args), loop=loop)
+    gateway = Gateway(**kwargs, loop=loop)
+
     try:
-        asyncio.run(main())
-    except asyncio.CancelledError:  # TODO: rubbish
+        asyncio.run(gateway.start())
+    except asyncio.CancelledError:  # TODO: rubbish workaround
         pass
+
+
+if __name__ == "__main__":
+    cli()
