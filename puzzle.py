4e6e1af9f8fe8e9ba6da02a6c35577a8928ba222
David Bonnes <david@bonnes.me>
2021-01-08 16:59:04 +0000

    tweaks

--- puzzle.py
+++ puzzle.py
@@ -6,10 +6,11 @@
     evohome_rf is used to parse/process Honeywell's RAMSES-II packets.
     """
 import asyncio
-from datetime import datetime as dt
+from datetime import datetime as dt, timedelta as td
 import json
 import shutil
 import sys
+from threading import Lock
 from typing import Tuple
 
 import click
@@ -31,6 +32,9 @@ from evohome_rf.command import Command, Priority
 from evohome_rf.helpers import dts_to_hex
 from evohome_rf.schema import USE_NAMES
 
+count_lock = Lock()
+count_rcvd = 0
+
 ALLOW_LIST = "allowlist"
 DEBUG_MODE = "debug_mode"
 EXECUTE_CMD = "execute_cmd"
@@ -44,10 +48,10 @@ DONT_CREATE_MESSAGES = 3
 DONT_CREATE_ENTITIES = 2
 DONT_UPDATE_ENTITIES = 1
 
-DEFAULT_INTERVAL = 0.5  # should be 3
+DEFAULT_INTERVAL = 0.5  # should be 5
 
 LOWER_FREQ = 0x216200
-MIDDLE_FREQ = 0x21656A
+BASIC_FREQ = 0x21656A
 UPPER_FREQ = 0x216800
 
 DEBUG_ADDR = "0.0.0.0"
@@ -131,11 +135,7 @@ class PortCommand(click.Command):
 
 @click.command(cls=PortCommand)
 @click.option(
-    "-c",
-    "--count",
-    type=int,
-    default=10,
-    help="number of packets to listen for"
+    "-c", "--count", type=int, default=10, help="number of packets to listen for"
 )
 @click.option(
     "-i",
@@ -161,7 +161,7 @@ def tune(obj, **kwargs):
     "--count",
     type=int,
     default=0,
-    help="number of packets to send (0 is unlimited)"
+    help="number of packets to send (0 is unlimited)",
 )
 @click.option(
     "-i",
@@ -186,16 +186,14 @@ async def puzzle_cast(gwy, pkt_protocol, interval=None, count=0, length=48, **kw
         dtm = f"{msg.dtm:%H:%M:%S.%f}"[:-3]
         print(f"{Fore.GREEN}{dtm} {msg}"[:CONSOLE_COLS])
 
-    async def _periodic(counter):
-        payload = f"7F{dts_to_hex(dt.now())}7F{counter % 0x10000:04X}7F{int_hex}7F"
-        payload = payload.ljust(length * 2, 'F')
+    async def _periodic(ordinal):
+        payload = f"7F{dts_to_hex(dt.now())}7F{ordinal % 0x10000:04X}7F{int_hex}7F"
+        payload = payload.ljust(length * 2, "F")
 
         qos = {"priority": Priority.ASAP, "retries": 0}
         await msg_protocol.send_data(
             Command(" I", "63:262142", "7FFF", payload, qos=qos)
         )
-
-        await asyncio.sleep(interval)
 
     msg_protocol, _ = gwy.create_client(print_message)
 
@@ -204,11 +202,13 @@ async def puzzle_cast(gwy, pkt_protocol, interval=None, count=0, length=48, **kw
     if count <= 0:
         counter = 0
         while True:
-            await _periodic(counter)
+            asyncio.create_task(_periodic(counter))
+            await asyncio.sleep(interval)
             counter += 1
     else:
         for counter in range(count):
-            await _periodic(counter)
+            asyncio.create_task(_periodic(counter))
+            await asyncio.sleep(interval)
 
 
 async def puzzle_tune(gwy, pkt_protocol, interval=None, count=0, **kwargs):
@@ -216,10 +216,71 @@ async def puzzle_tune(gwy, pkt_protocol, interval=None, count=0, **kwargs):
         dtm = f"{msg.dtm:%H:%M:%S.%f}"[:-3]
         print(f"{Fore.CYAN}{dtm} {msg}"[:CONSOLE_COLS])
 
-    def process_message(msg) -> None:
-        print_message(msg)
+    async def set_freq(frequency):
+        hex = f"{frequency:06X}"
+        data = f"!C {hex[:2]} {hex[2:4]} {hex[4:]}"
+        await pkt_protocol._write_data(bytes(data.encode("ascii")))
 
+    def process_message(msg) -> None:
+        global count_rcvd
+
+        count_lock.acquire()
+        count_rcvd += 1
+        count_lock.release()
+
+        if msg.payload["interval"] != interval:
+            raise RuntimeError("Intervals don't match")
+
+    async def check_reception(freq, count=3) -> bool:
+        global count_rcvd
+        await set_freq(freq)
+
+        await pkt_protocol._write_data(bytes("!V\r\n".encode("ascii")))  # TODO: remove
+
+        count_lock.acquire()
+        count_rcvd = 0
+        count_lock.release()
+
+        print(
+            f"checking 0x{freq:04X} for {interval * count} secs, expecting {count} pkts"
+        )
+
+        await asyncio.sleep(interval * count)
+
+        result = count_rcvd / count
+        print(f"result = {result} ({count_rcvd}/{count} pkts received)")
+        return result
+
+    async def binary_chop(start, target, threshold=0) -> Tuple[int, float]:  # 1, 2
+        freq = start
+        while True:
+            result = await check_reception(freq)
+            if result > threshold:
+                return freq, result
+            new_freq = int((freq + target) / 2)
+            if new_freq in (start, freq, target):
+                return freq, result
+            freq = new_freq
+
+    gwy.create_client(print_message)
     gwy.create_client(process_message)
+
+    dtm_expires = dt.now() + td(seconds=5)
+    while dt.now() < dtm_expires:
+        await asyncio.sleep(0.1)
+        if pkt_protocol._has_initialized:
+            break
+    else:
+        raise RuntimeError("Can't find serial interface")
+
+    # if not await check_reception(BASIC_FREQ):
+    #     raise RuntimeError("Can't find beacon")
+
+    lower_freq, result1 = await binary_chop(LOWER_FREQ, BASIC_FREQ + 1)
+    print(f"0x{lower_freq:04X}", result1)
+
+    upper_freq, result2 = await binary_chop(UPPER_FREQ, lower_freq)
+    print(f"RESULT = 0x{upper_freq:04X} (0x{lower_freq}, {result1}, {result2})")
 
 
 async def main(lib_kwargs, **kwargs):
@@ -236,15 +297,15 @@ async def main(lib_kwargs, **kwargs):
 
     gwy = Gateway(lib_kwargs[CONFIG].pop(SERIAL_PORT, None), **lib_kwargs)
 
-    task = asyncio.create_task(gwy.start())
+    gwy_task = asyncio.create_task(gwy.start())
     while gwy.pkt_protocol is None:
         await asyncio.sleep(0.05)
     pkt_protocol = gwy.pkt_protocol
 
     if kwargs[COMMAND] == "cast":
-        asyncio.create_task(puzzle_cast(gwy, pkt_protocol, **kwargs))
+        task = asyncio.create_task(puzzle_cast(gwy, pkt_protocol, **kwargs))
     else:  # kwargs[COMMAND] == "tune":
-        asyncio.create_task(puzzle_tune(gwy, pkt_protocol, **kwargs))
+        task = asyncio.create_task(puzzle_tune(gwy, pkt_protocol, **kwargs))
 
     try:  # main code here
         await task
