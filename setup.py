c3533524e8947aac06c80b06bfb34ea9842a10db
David Bonnes <david@bonnes.me>
2021-03-10 20:49:07 +0000

    add workflow

--- setup.py
+++ setup.py
@@ -1,31 +1,53 @@
-import setuptools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+"""The setup.py file."""
+
+import os
+from setuptools import find_packages, setup
+from setuptools.command.install import install
+import sys
+
 
 from evohome_rf import __version__ as VERSION
+
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our VERSION."""
+
+    def run(self):
+        tag = os.getenv("CIRCLE_TAG")
+        if tag != VERSION:
+            info = "Git tag: {tag} does not match the version of this pkg: {VERSION}"
+            sys.exit(info)
+
 
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
-setuptools.setup(
+setup(
     name="evohome-rf",
-    version=VERSION,
+    description="An interface for Honeywell RF (RAMSES II), as used by HVAC, CH/DHW.",
+    keywords=["ramses", "evohome", "sunidal", "chronotherm", "hometronics"],
     author="David Bonnes",
     author_email="zxdavb@gmail.com",
-    description="A protcol analyser for Honeywell RF (RAMSES II), as used by evohome.",
+    url="https://github.com/zxdavb/evohome_rf",
+    download_url="{url}/archive/{VERSION}.tar.gz",
+    install_requires=[list(val.strip() for val in open("requirements.txt"))],
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/zxdavb/evohome_rf",
-    # download_url=f"https://github.com/zxdavb/evohome_rf/archive/{VERSION}.tar.gz",
-    packages=["evohome_rf"],
-    # packages=setuptools.find_packages(exclude=['test']),
-    keywords=["evohome", "ramses"],
-    # install_requires=[list(val.strip() for val in open("requirements.txt"))],
-    install_requires=["pyserial-asyncio==0.5", "voluptuous>=0.11.7"],
+    packages=find_packages(exclude=["test", "docs"]),
+    version=VERSION,
+    license="MIT",
     python_requires=">=3.7",
     classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Home Automation",
     ],
+    cmdclass={
+        "verify": VerifyVersionCommand,
+    },
 )
