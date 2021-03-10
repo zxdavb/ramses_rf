8ebfa20a92c2a7e4d51d2c56b131c72a757c4c92
David Bonnes <david@bonnes.me>
2021-03-10 23:12:47 +0000

    broken merge

--- evohome_rf/transport.py
+++ evohome_rf/transport.py
@@ -39,7 +39,10 @@ from .schema import (
 )
 from .version import __version__
 
+_dev_mode_ = True
+
 DEV_MODE = __dev_mode__ and False
+DEV_MODE = _dev_mode_ and False
 
 ERR_MSG_REGEX = re.compile(r"^([0-9A-F]{2}\.)+$")
 
