441d9f519048ce4dca62f6f9d05e4b2b3101ba62
David Bonnes <david@bonnes.me>
2021-08-12 21:09:11 +0100

    doctweak

--- ramses_rf/transport.py
+++ ramses_rf/transport.py
@@ -349,7 +349,7 @@ class PacketProtocolBase(asyncio.Protocol):
     def _is_wanted(self, src_addr, dst_addr) -> bool:
         """Parse the packet, return True if the packet is not to be filtered out."""
         pkt_addrs = {src_addr, dst_addr}
-        if any(d.type == "18" for d in pkt_addrs):  # TODO: use GWY's full addr
+        if any(d.type == "18" for d in pkt_addrs):  # TODO: use HGI's full addr
             return True
         wanted = not self._include or any(d.id in self._include for d in pkt_addrs)
         return wanted or not all(d.id not in self._exclude for d in pkt_addrs)
