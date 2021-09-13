22c1eaa70f68aa3c8eb917cbd73f2243494b6148
David Bonnes <david@bonnes.me>
2021-09-13 18:14:39 +0100

    0.14.13 - bugfix discover/faking/has_battery, tweak BDR.repr

--- ramses_rf/protocol/version.py
+++ ramses_rf/protocol/version.py
@@ -6,5 +6,5 @@
     The RAMSES-II protocol (messages, packets/commands).
 """
 
-__version__ = "0.14.12"
+__version__ = "0.14.13"
 VERSION = __version__
--- ramses_rf/version.py
+++ ramses_rf/version.py
@@ -6,5 +6,5 @@
     The RAMSES-II system (controllers, zones, devices).
 """
 
-__version__ = "0.14.12"
+__version__ = "0.14.13"
 VERSION = __version__
