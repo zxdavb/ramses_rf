35fc7e1f6be634a5d183033f1db500b8d1a77c32
David Bonnes <david@bonnes.me>
2021-09-26 17:04:08 +0100

    0.14.19 - DHW temp convergence, has_payload to Frame, delint

--- ramses_rf/protocol/version.py
+++ ramses_rf/protocol/version.py
@@ -6,5 +6,5 @@
     The RAMSES-II protocol (messages, packets/commands).
 """
 
-__version__ = "0.14.18"
+__version__ = "0.14.19"
 VERSION = __version__
--- ramses_rf/version.py
+++ ramses_rf/version.py
@@ -6,5 +6,5 @@
     The RAMSES-II system (controllers, zones, devices).
 """
 
-__version__ = "0.14.18"
+__version__ = "0.14.19"
 VERSION = __version__
