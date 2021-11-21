ed5e6a670f4b1570c504a364a69822b7cf3951ae
David Bonnes <zxdavb@bonnes.me>
2021-11-21 15:13:00 +0000

    add test

--- .tests/tests.sh
+++ .tests/tests.sh
@@ -3,6 +3,7 @@ python client.py execute /dev/ttyACM0 --get-schedule 01:054173 00
 python client.py execute /dev/ttyACM0 --get-schedule 01:054173 HW
 
 python client.py monitor /dev/ttyACM0 -x "RQ 01:054173 1F09 00"
+python client.py monitor /dev/ttyUSB0 -X scan_full 32:168090
 
 
 python client.py execute 'alt:///dev/ttyUSB0?class=PosixPollSerial' --get-faults 01:145038
