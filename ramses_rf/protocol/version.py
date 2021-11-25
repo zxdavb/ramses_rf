fa87cb32375d0095cfc1136ea38f7d0e20edb889
David Bonnes <zxdavb@bonnes.me>
2021-11-25 16:41:34 +0000

    bump to 0.16.17 - DHW_HACK

--- ramses_rf/protocol/version.py
+++ ramses_rf/protocol/version.py
@@ -6,5 +6,5 @@
     The RAMSES-II protocol (messages, packets/commands).
 """
 
-__version__ = "0.16.16"
+__version__ = "0.16.17"
 VERSION = __version__
--- ramses_rf/version.py
+++ ramses_rf/version.py
@@ -6,5 +6,5 @@
     The RAMSES-II system (controllers, zones, devices).
 """
 
-__version__ = "0.16.16"
+__version__ = "0.16.17"
 VERSION = __version__
