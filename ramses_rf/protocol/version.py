4b43ab7ee19188bb5c029d7d08387d3ded374498
David Bonnes <zxdavb@bonnes.me>
2021-11-27 19:14:51 +0000

    0.16.22 - Bugfix get_relay_demand

--- ramses_rf/protocol/version.py
+++ ramses_rf/protocol/version.py
@@ -6,5 +6,5 @@
     The RAMSES-II protocol (messages, packets/commands).
 """
 
-__version__ = "0.16.21"
+__version__ = "0.16.22"
 VERSION = __version__
--- ramses_rf/version.py
+++ ramses_rf/version.py
@@ -6,5 +6,5 @@
     The RAMSES-II system (controllers, zones, devices).
 """
 
-__version__ = "0.16.21"
+__version__ = "0.16.22"
 VERSION = __version__
