95d556de6586a11c49b27942708e03483c63c15b
David Bonnes <zxdavb@bonnes.me>
2021-11-29 17:09:46 +0000

    doctweak

--- ramses_rf/protocol/ramses.py
+++ ramses_rf/protocol/ramses.py
@@ -271,7 +271,7 @@ RAMSES_CODES = {  # rf_unknown
     },
     _10E0: {  # device_info
         NAME: "device_info",
-        I_: r"^00[0-9A-F]{30,}$",  # NOTE: RP is same
+        I_: r"^00[0-9A-F]{30,}$",  # r"^[0-9A-F]{32,}$" might be OK
         RQ: r"^00$",  # NOTE: will accept [0-9A-F]{2}
         # RP: r"^[0-9A-F]{2}([0-9A-F]){30,}$",  # NOTE: indx same as RQ
         EXPIRES: False,
