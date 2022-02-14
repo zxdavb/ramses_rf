e11d8ca7232890936081a79de9a2ad6f476bbe2d
David Bonnes <zxdavb@bonnes.me>
2022-02-14 22:24:44 +0000

    tweak 12A0, UFC

--- ramses_rf/protocol/ramses.py
+++ ramses_rf/protocol/ramses.py
@@ -335,7 +335,7 @@ RAMSES_CODES = {  # rf_unknown
     },
     _12A0: {  # indoor_humidity
         NAME: "indoor_humidity",
-        I_: r"^00[0-9A-F]{2}[0-9A-F]{8}?$",  # TODO: (00)? at end - needs confirming
+        I_: r"^00[0-9A-F]{2}([0-9A-F]{8}(00))?$",  # TODO: (00)? at end - needs confirming
         EXPIRES: td(hours=1),
     },
     _12B0: {  # window_state  (HVAC % window open)
@@ -820,6 +820,7 @@ RAMSES_DEVICES_CLASS = {
         _0008: {I_: {}},
         _000A: {RP: {}},
         _000C: {RP: {}},
+        _1FC9: {I_: {}},
         _10E0: {I_: {}, RP: {}},
         _22C9: {I_: {}},  # NOTE: No RP
         _22D0: {I_: {}, RP: {}},
