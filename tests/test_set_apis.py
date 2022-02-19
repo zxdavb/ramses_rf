9f1e3d7831bc73237679474cd5ff9399da706bfc
David Bonnes <zxdavb@bonnes.me>
2022-02-19 02:30:44 +0000

    add more tests

--- tests/test_set_apis.py
+++ tests/test_set_apis.py
@@ -16,27 +16,27 @@ from ramses_rf.protocol.helpers import str_from_hex
 from ramses_rf.protocol.message import Message
 from ramses_rf.protocol.packet import Packet
 
-PKT_0004_GOOD = (
+SET_0004_GOOD = (
     "...  W --- 18:000730 01:145038 --:------ 0004 022 00004D617374657220426564726F6F6D000000000000",
     "...  W --- 18:000730 01:145038 --:------ 0004 022 05005468697320497320412056657279204C6F6E6767",
 )
-PKT_0004_FAIL = (
+SET_0004_FAIL = (
     "...  W --- 18:000730 01:145038 --:------ 0004 022 05000000000000000000000000000000000000000000",  # name is None
     "...  W --- 18:000730 01:145038 --:------ 0004 022 05005468697320497320412056657279204C6F6E6720",  # trailing space
 )
-PKT_000A_GOOD = (
+SET_000A_GOOD = (
     "...  W --- 18:000730 01:145038 --:------ 000A 006 010001F40DAC",
     "...  W --- 18:000730 01:145038 --:------ 000A 006 031001F409C4",
     "...  W --- 18:000730 01:145038 --:------ 000A 006 050201F40898",
 )
-PKT_1030_GOOD = (
+SET_1030_GOOD = (
     "...  W --- 18:000730 01:145038 --:------ 1030 016 01C80137C9010FCA0196CB010FCC0101",
 )
-PKT_10A0_GOOD = (
+SET_10A0_GOOD = (
     "...  W --- 01:123456 07:031785 --:------ 10A0 006 0015180001F4",
     "...  W --- 01:123456 07:031785 --:------ 10A0 006 0015180001F4",
 )
-PKT_1100_GOOD = (
+SET_1100_GOOD = (
     "...  W --- 01:145038 13:035462 --:------ 1100 008 00240414007FFF01",
     "...  W --- 01:145038 13:163733 --:------ 1100 008 000C14000000C801",  # min_off_time 0
     "...  W --- 01:145038 13:163733 --:------ 1100 008 00180400007FFF01",  # min_off_time 0
@@ -48,23 +48,23 @@ PKT_1100_GOOD = (
     "...  W --- 01:145038 13:035462 --:------ 1100 008 FC083C14007FFF01",  # cycle_rate 2
     "...  W --- 01:145038 13:035462 --:------ 1100 008 FC083C00007FFF01",  # cycle_rate 2
 )
-PKT_1F41_GOOD = (
+SET_1F41_GOOD = (
     "...  W --- 18:000730 01:050858 --:------ 1F41 012 000104FFFFFF0509160607E5",
     "...  W --- 18:000730 01:050858 --:------ 1F41 006 000000FFFFFF",
     "...  W --- 18:000730 01:050858 --:------ 1F41 012 000104FFFFFF2F0E0D0B07E5",
     "...  W --- 18:000730 01:050858 --:------ 1F41 012 000104FFFFFF19100D0B07E5",
 )
-PKT_1100_FAIL = (
+SET_1100_FAIL = (
     "...  W --- 01:145038 13:163733 --:------ 1100 008 000C1400007FFF01",  # no domain_id
 )
-PKT_2309_GOOD = (
+SET_2309_GOOD = (
     "...  W --- 18:000730 01:145038 --:------ 2309 003 00047E",
     "...  W --- 18:000730 01:145038 --:------ 2309 003 0101F4",
 )
-PKT_2309_FAIL = (
+SET_2309_FAIL = (
     "...  W --- 18:000730 01:145038 --:------ 2309 003 017FFF",  # temp is None - should be good?
 )
-PKT_2349_GOOD = (
+SET_2349_GOOD = (
     "...  W --- 18:005567 01:223036 --:------ 2349 007 037FFF00FFFFFF",
     "...  W --- 22:015492 01:076010 --:------ 2349 007 0101F400FFFFFF",
     "...  W --- 18:000730 01:145038 --:------ 2349 007 06028A01FFFFFF",
@@ -72,7 +72,7 @@ PKT_2349_GOOD = (
     "...  W --- 18:000730 01:050858 --:------ 2349 013 06096004FFFFFF240A050107E6",
     "...  W --- 18:000730 01:050858 --:------ 2349 013 02096004FFFFFF1B0D050107E6",
 )
-PKT_2E04_GOOD = (
+SET_2E04_GOOD = (
     # "...  W --- 30:258720 01:073976 --:------ 2E04 008 0000000000000000",
     "...  W --- 30:258720 01:073976 --:------ 2E04 008 00FFFFFFFFFFFF00",
     "...  W --- 30:258720 01:073976 --:------ 2E04 008 01FFFFFFFFFFFF00",
@@ -80,7 +80,7 @@ PKT_2E04_GOOD = (
     "...  W --- 30:258720 01:073976 --:------ 2E04 008 020B011A0607E401",
     "...  W --- 30:258720 01:073976 --:------ 2E04 008 0521011A0607E401",
 )
-PKT_313F_GOOD = (
+SET_313F_GOOD = (
     "...  W --- 30:258720 01:073976 --:------ 313F 009 006000320C040207E6",
     "...  W --- 30:258720 01:073976 --:------ 313F 009 0060011E09010707E6",
     "...  W --- 30:258720 01:073976 --:------ 313F 009 006002210D080C07E5",
@@ -89,7 +89,7 @@ PKT_313F_GOOD = (
 )
 
 
-class TestWriteApis(unittest.IsolatedAsyncioTestCase):
+class TestSetApis(unittest.IsolatedAsyncioTestCase):
 
     _gwy = Gateway(None, loop=asyncio.get_event_loop(), config={})
 
@@ -126,28 +126,28 @@ class TestWriteApis(unittest.IsolatedAsyncioTestCase):
             # self.assertEqual(cmd.payload, pkt.payload)
 
     async def test_0004(self):
-        await self._test_api(Command.set_zone_name, PKT_0004_GOOD)
+        await self._test_api(Command.set_zone_name, SET_0004_GOOD)
         # self.assertEqual(str_from_hex(cmd.payload[4:]), msg.payload["name"])
 
     async def test_000a(self):
-        await self._test_api(Command.set_zone_config, PKT_000A_GOOD)
+        await self._test_api(Command.set_zone_config, SET_000A_GOOD)
 
     async def test_1030(self):  # TODO: no W|1030 seen in the wild
-        await self._test_api(Command.set_mix_valve_params, PKT_1030_GOOD)
+        await self._test_api(Command.set_mix_valve_params, SET_1030_GOOD)
 
     async def test_10a0(self):  # TODO: no W|10A0 seen in the wild
-        await self._test_api(Command.set_dhw_params, PKT_10A0_GOOD)
+        await self._test_api(Command.set_dhw_params, SET_10A0_GOOD)
 
-    async def test_1100(self):  # bespoke
-        # await self._test_api(Command.set_tpi_params, PKT_1100_GOOD)
-        for pkt_line in PKT_1100_GOOD:
+    async def test_1100(self):  # NOTE: bespoke
+        # await self._test_api(Command.set_tpi_params, SET_1100_GOOD)
+        for pkt_line in SET_1100_GOOD:
             pkt = Packet.from_port(self._gwy, dt.now(), pkt_line)
 
             self.assertEqual(str(pkt), pkt_line[4:])
 
             msg = Message(self._gwy, pkt)
 
-            domain_id = msg.payload.pop("domain_id", "00")
+            domain_id = msg.payload.pop("domain_id", None)
             cmd = Command.set_tpi_params(
                 msg.dst.id, domain_id, **{k: v for k, v in msg.payload.items() if k[:1] != "_"}
             )
@@ -155,19 +155,19 @@ class TestWriteApis(unittest.IsolatedAsyncioTestCase):
             self.assertEqual(cmd.payload, pkt.payload)
 
     async def test_1f41(self):
-        await self._test_api(Command.set_dhw_mode, PKT_1F41_GOOD)
+        await self._test_api(Command.set_dhw_mode, SET_1F41_GOOD)
 
     async def test_2309(self):
-        await self._test_api(Command.set_zone_setpoint, PKT_2309_GOOD)
+        await self._test_api(Command.set_zone_setpoint, SET_2309_GOOD)
 
     async def test_2349(self):
-        await self._test_api(Command.set_zone_mode, PKT_2349_GOOD)
+        await self._test_api(Command.set_zone_mode, SET_2349_GOOD)
 
     async def test_2e04(self):
-        await self._test_api(Command.set_system_mode, PKT_2E04_GOOD)
+        await self._test_api(Command.set_system_mode, SET_2E04_GOOD)
 
-    async def test_313f(self):  # bespoke
-        for pkt_line in PKT_313F_GOOD:
+    async def test_313f(self):  # NOTE: bespoke
+        for pkt_line in SET_313F_GOOD:
             pkt = Packet.from_port(self._gwy, dt.now(), pkt_line)
 
             self.assertEqual(str(pkt)[:4], pkt_line[4:8])
