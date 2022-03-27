101877ac3b3f8203ba4ad65466b8f84f7e07dbd3
David Bonnes <zxdavb@bonnes.me>
2022-03-27 13:15:19 +0100

    add clean to helps

--- ramses_rf/helpers.py
+++ ramses_rf/helpers.py
@@ -8,6 +8,32 @@ import re
 from inspect import iscoroutinefunction
 
 
+def clean(value: dict, keep_falsys: bool = False, keep_hints: bool = False) -> dict:
+    """Walk through a dict and remove all the meaningless items.
+
+    Specifically, removes items with:
+    - uwanted keys (starting with '_')
+    - falsey values
+    """
+
+    def walk(node):
+        if isinstance(node, dict):
+            return {
+                k: walk(v)
+                for k, v in node.items()
+                if (keep_hints or k[:1] != "_") and (keep_falsys or walk(v))
+            }
+        elif isinstance(node, list):
+            return [walk(x) for x in node if x]
+        else:
+            return node
+
+    if not isinstance(value, dict):
+        raise TypeError("value is not a dict")
+
+    return walk(value)
+
+
 def schedule_task(fnc, *args, delay=None, period=None, **kwargs) -> asyncio.Task:
     """Start a coro after delay seconds."""
 
