b414af1878e80cb5a285c400bbd7f9bb18590d2d
David Bonnes <zxdavb@bonnes.me>
2022-04-14 21:25:39 +0100

    refactor shedloads 2

---
+++ tests/wip/_test_eavesdrop_htg_control.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+"""RAMSES RF - a RAMSES-II protocol decoder & analyser.
+
+Test eavesdropping of the heating controller.
+"""
+
+# import unittest
