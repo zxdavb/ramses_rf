07db0c761caa7f4234e1b3535a2906495c3cba31
David Bonnes <zxdavb@bonnes.me>
2022-07-08 21:41:57 +0100

    tweak test

---
+++ tests/wip/_test_schedule_set.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+"""RAMSES RF - a RAMSES-II protocol decoder & analyser.
+
+Test the Schedule functions.
+"""
+
+import asyncio
+import json
+import unittest
+
+from ramses_rf import Gateway
+from ramses_rf.discovery import SET_SCHED, spawn_scripts
+from tests.common import TEST_DIR
+
+WORK_DIR = f"{TEST_DIR}/schedules"
+
+
+class TestSchedule(unittest.IsolatedAsyncioTestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.maxDiff = None
+        self.gwy = None
+
+    async def test_schedule_set(self):
+        with open(f"{TEST_DIR}/schemas/schema_000.log") as f:
+            self.gwy = Gateway(
+                None, input_file=f, config={}, loop=self._asyncioTestLoop
+            )
+            await self.gwy.start()
+
+            # self.gwy.config.disable_sending = False
+
+        with open(f"{WORK_DIR}/schedule.json") as f:
+            schedule = json.load(f)
+
+        with open(f"{WORK_DIR}/schedule.json") as f:
+            await asyncio.gather(
+                *spawn_scripts(self.gwy, **{SET_SCHED: ("01:123456", f)})
+            )
+
+        zone = self.gwy.system_by_id["01:145038"].zone_by_idx["01"]
+        result = {"zone_idx": zone.idx, "schedule": zone.schedule}
+
+        self.assertEqual(result, schedule)
+
+
+if __name__ == "__main__":
+    unittest.main()
