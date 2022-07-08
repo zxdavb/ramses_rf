f1052e7c8b7347ee5b857496a472306e64f27ee1
David Bonnes <zxdavb@bonnes.me>
2022-07-08 15:33:04 +0100

    tweak parser tests

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -13,7 +13,7 @@ from ramses_rf.protocol.packet import Packet
 from tests.common import gwy  # noqa: F401
 from tests.common import TEST_DIR
 
-WORK_DIR = f"{TEST_DIR}/parsers_heat"
+WORK_DIR = f"{TEST_DIR}/parsers"
 
 
 def id_fnc(param):
@@ -25,14 +25,16 @@ def pytest_generate_tests(metafunc):
 
 
 def _proc_log_line(gwy, pkt_line):  # noqa: F811
-    pkt_line, pkt_dict = pkt_line.split("#", maxsplit=1)
-    if pkt_line[27:].strip():
+    pkt_line, pkt_dict = list(
+        map(str.strip, pkt_line.split("#", maxsplit=1) + [""])[:2]
+    )
+
+    if pkt_line:
         msg = Message(gwy, Packet.from_file(gwy, pkt_line[:26], pkt_line[27:]))
-        assert msg.payload == eval(pkt_dict)
+        assert not pkt_dict or msg.payload == eval(pkt_dict)
 
 
 def test_parsers_from_log_files(gwy, f_name):  # noqa: F811
     with open(f_name) as f:
         while line := (f.readline()):
-            if line.strip():
-                _proc_log_line(gwy, line)
+            _proc_log_line(gwy, line)
