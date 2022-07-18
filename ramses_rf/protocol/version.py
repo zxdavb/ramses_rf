7e029c0550bdd2207c3208e26004cf46f4264b80
David Bonnes <zxdavb@bonnes.me>
2022-07-18 16:36:06 +0100

    bump to 0.20.15

--- ramses_rf/protocol/version.py
+++ ramses_rf/protocol/version.py
@@ -6,5 +6,5 @@
 The RAMSES-II protocol (messages, packets/commands).
 """
 
-__version__ = "0.20.14"
+__version__ = "0.20.15"
 VERSION = __version__
--- ramses_rf/version.py
+++ ramses_rf/version.py
@@ -6,5 +6,5 @@
 The RAMSES-II system (controllers, zones, devices).
 """
 
-__version__ = "0.20.14"
+__version__ = "0.20.15"
 VERSION = __version__
