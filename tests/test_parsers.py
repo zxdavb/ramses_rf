9b88c90a009965e19fd279c48c3e770fdd86839b
David Bonnes <zxdavb@bonnes.me>
2022-07-31 00:30:57 +0100

    test payload meta data

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -15,6 +15,12 @@ from tests.common import TEST_DIR
 
 WORK_DIR = f"{TEST_DIR}/parsers"
 
+HAS_ARRAY = "has_array"
+HAS_IDX = "has_idx"
+HAS_PAYLOAD = "has_payload"
+IS_FRAGMENT = "is_fragment"
+META_KEYS = (HAS_ARRAY, HAS_IDX, HAS_PAYLOAD, IS_FRAGMENT)
+
 
 def id_fnc(param):
     return PurePath(param).name
@@ -29,9 +35,25 @@ def _proc_log_line(gwy, pkt_line):  # noqa: F811
         map(str.strip, pkt_line.split("#", maxsplit=1) + [""])
     )
 
-    if pkt_line:
-        msg = Message(gwy, Packet.from_file(gwy, pkt_line[:26], pkt_line[27:]))
-        assert not pkt_dict or msg.payload == eval(pkt_dict)
+    if not pkt_line:
+        return
+
+    msg = Message(gwy, Packet.from_file(gwy, pkt_line[:26], pkt_line[27:]))
+
+    if not pkt_dict:
+        return
+    pkt_dict = eval(pkt_dict)
+
+    if isinstance(pkt_dict, list) or not any(k for k in pkt_dict if k in META_KEYS):
+        assert msg.payload == pkt_dict
+        return
+
+    assert HAS_ARRAY not in pkt_dict or msg._has_array == pkt_dict[HAS_ARRAY]
+    assert HAS_IDX not in pkt_dict or msg._pkt._idx == pkt_dict[HAS_IDX]
+    assert HAS_PAYLOAD not in pkt_dict or msg._has_payload == pkt_dict[HAS_PAYLOAD]
+    assert (
+        IS_FRAGMENT not in pkt_dict or bool(msg._is_fragment) == pkt_dict[IS_FRAGMENT]
+    )
 
 
 def test_parsers_from_log_files(gwy, f_name):  # noqa: F811
