dee350db7156b67b01680b593f57192b49680b82
David Bonnes <zxdavb@bonnes.me>
2022-08-16 11:05:05 +0100

    dev tweak

--- ramses_rf/processor.py
+++ ramses_rf/processor.py
@@ -49,7 +49,7 @@ CODE_NAMES = {k: v["name"] for k, v in CODES_SCHEMA.items()}
 MSG_FORMAT_10 = "|| {:10s} | {:10s} | {:2s} | {:16s} | {:^4s} || {}"
 MSG_FORMAT_18 = "|| {:18s} | {:18s} | {:2s} | {:16s} | {:^4s} || {}"
 
-DEV_MODE = __dev_mode__ or True  # set True for useful Tracebacks
+DEV_MODE = __dev_mode__ and False  # or True  # set True for useful Tracebacks
 
 _LOGGER = logging.getLogger(__name__)
 if DEV_MODE:
