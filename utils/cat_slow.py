6cfc4ff8c3672bec4ccc8a978ee57d09e2051a04
David Bonnes <zxdavb@bonnes.me>
2022-08-19 12:43:20 +0100

    doctweak

--- utils/cat_slow.py
+++ utils/cat_slow.py
@@ -4,6 +4,9 @@
 import argparse
 from time import sleep
 
+# python utils/cat_slow.py -i packet.log | tee /dev/pts/0
+# cat packet.log | cut -d ' ' -f 2- | unix2dos | pv --quiet --line-mode --rate-limit 1 | tee /dev/pts/3
+
 parser = argparse.ArgumentParser(description="Echo a file slowly")
 parser.add_argument("-i", "--input-file", type=argparse.FileType("r"), default="-")
 parser.add_argument("-d", "--delay-in-ms", type=int, default="100")
