db2293caa54fc15ccb7690dc59fb28d572ddce34
David Bonnes <zxdavb@bonnes.me>
2023-07-23 18:21:00 +0100

    tweak test

--- tests/test_ramses_schema.py
+++ tests/test_ramses_schema.py
@@ -23,9 +23,12 @@ from ramses_rf.protocol.ramses import (
 )
 
 
-def test_code_count():  # TODO make ==, not >=
-    assert len(Code) >= len(CODES_SCHEMA)
+def test_code_counts():
+    """All known command codes should be in the schema & vice-versa."""
+
+    # assert len(Code) == len(CODES_SCHEMA)
     assert not [c for c in CODES_SCHEMA if c not in Code]
+    assert not [c for c in Code if c not in CODES_SCHEMA]
 
 
 def test_verb_code_pairs():
@@ -37,7 +40,7 @@ def test_verb_code_pairs():
 
 
 def test_device_heat_slugs():
-    """Every device slug should have an entry in it domain's _DEV_KLASSES_*."""
+    """Every Heat device slug should have an entry in it domain's _DEV_KLASSES_*."""
 
     assert not [s for s in _DEV_KLASSES_HEAT if s not in HEAT_CLASS_BY_SLUG]
     assert not [
@@ -48,7 +51,7 @@ def test_device_heat_slugs():
 
 
 def test_device_hvac_slugs():
-    """Every device slug should have an entry in it domain's _DEV_KLASSES_*."""
+    """Every HVAC device slug should have an entry in it domain's _DEV_KLASSES_*."""
 
     assert not [s for s in _DEV_KLASSES_HVAC if s not in HVAC_CLASS_BY_SLUG]
     assert not [
@@ -59,17 +62,17 @@ def test_device_hvac_slugs():
 
 
 def assert_codes_idx_mutex(mutex_list, other_list):
+    """Assert the two lists are mutually exclusive."""
+
     codes = sorted(c for c in mutex_list if c in other_list)
-    assert codes == []
+    assert not codes
 
 
 def test_codes_idx_mutex():
     """Every code should be in one of the three CODE_IDX_* constants."""
 
     codes_idx_all = CODE_IDX_COMPLEX + CODE_IDX_NONE + CODE_IDX_SIMPLE
-    assert_codes_idx_mutex(
-        [c for c in CODES_SCHEMA if c not in codes_idx_all], CODES_SCHEMA
-    )
+    assert not [c for c in CODES_SCHEMA if c not in codes_idx_all]
 
 
 def test_codes_idx_complex_mutex():
@@ -90,7 +93,7 @@ def test_codes_idx_simple_mutex():
     assert_codes_idx_mutex(CODE_IDX_SIMPLE, CODE_IDX_NONE + CODE_IDX_COMPLEX)
 
 
-def _test_codes_mutex():
+def test_codes_mutex():
     assert_codes_idx_mutex(RQ_IDX_ONLY, CODE_IDX_NONE)
 
 
@@ -98,14 +101,10 @@ RQ_IDX_NONE = [k for k, v in CODES_SCHEMA.items() if v.get(RQ, "")[:3] == "^00"]
 RQ_IDX_ONLY = [
     k
     for k, v in CODES_SCHEMA.items()
-    if k not in RQ_NO_PAYLOAD
-    and RQ in v
-    and (v[RQ] in (r"^0[0-9A-F]00$", r"^0[0-9A-F](00)?$"))
+    if k not in RQ_NO_PAYLOAD and (v.get(RQ) in (r"^0[0-9A-F]00$", r"^0[0-9A-F](00)?$"))
 ]
-RQ_IDX_ONLY.extend((Code._0418,))
 RQ_IDX_UNKNOWN = [
     k
     for k, v in CODES_SCHEMA.items()
     if k not in RQ_NO_PAYLOAD + RQ_IDX_ONLY and RQ in v
 ]
-# RQ_UNKNOWN.extend()
