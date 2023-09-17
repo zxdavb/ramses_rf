27b065c12628c90f29b70d2a10ace18192cd013a
David Bonnes <zxdavb@bonnes.me>
2023-09-17 10:29:24 +0100

    doctweak

--- tests_rf/test_protocol_fsm.py
+++ tests_rf/test_protocol_fsm.py
@@ -111,7 +111,7 @@ async def assert_protocol_state(
 def assert_protocol_state_detail(
     protocol: QosProtocol, cmd: Command, cmd_sends: int
 ) -> None:
-    assert cmd is None or protocol._context._is_active_cmd(cmd)  # duplicate test
+    assert (cmd is None) or protocol._context._is_active_cmd(cmd)  # duplicate test
     assert protocol._context.state.cmd is cmd  # duplicate of above
     assert protocol._context.state.cmd_sends == cmd_sends
     assert bool(cmd) is isinstance(protocol._context.state, (WantEcho, WantRply))
