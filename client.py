465799eb9bcb944275f55bd6aa96c625b2795921
David Bonnes <zxdavb@bonnes.me>
2023-11-15 20:17:08 +0000

    add client.py

--- client.py
+++ client.py
@@ -1,11 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
 """A CLI for the ramses_rf library.
 
 ramses_rf is used to parse/process Honeywell's RAMSES-II packets.
 """
-from __future__ import annotations
 
 try:
     from ramses_cli.client import main
@@ -16,7 +12,7 @@ except ModuleNotFoundError:
 
     sys.path.append(f"{os.path.dirname(__file__)}/src")
 
-    from ramses_cli.client import main
+    from ramses_cli.client import main  # type: ignore[import-not-found]
 
 if __name__ == "__main__":
     main()
