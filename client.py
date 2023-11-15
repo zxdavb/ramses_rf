ba7c33122c7a06407e7bb52f8dbe3f862677851e
David Bonnes <zxdavb@bonnes.me>
2023-11-15 17:32:44 +0000

    bugfix client.py

--- client.py
+++ client.py
@@ -7,7 +7,16 @@ ramses_rf is used to parse/process Honeywell's RAMSES-II packets.
 """
 from __future__ import annotations
 
-from ramses_cli.client import main
+try:
+    from ramses_cli.client import main
+
+except ModuleNotFoundError:
+    import os
+    import sys
+
+    sys.path.append(f"{os.path.dirname(__file__)}/src")
+
+    from ramses_cli.client import main
 
 if __name__ == "__main__":
     main()
