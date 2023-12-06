4c5dc616e8d6a503d448dafe40beb6a2e591a422
David Bonnes <zxdavb@bonnes.me>
2023-12-06 16:02:21 +0000

    doctweak

--- src/ramses_tx/version.py
+++ src/ramses_tx/version.py
@@ -1,4 +1,4 @@
-"""RAMSES RF - a RAMSES-II protocol decoder & analyser (transpor layer)."""
+"""RAMSES RF - a RAMSES-II protocol decoder & analyser (transport layer)."""
 
 __version__ = "0.30.4"
 VERSION = __version__
