ad6564dc0b88af19fde8a0e4922bda98b5186dfe
David Bonnes <zxdavb@bonnes.me>
2024-01-01 19:19:09 +0000

    reaftcor ensure_fakeable helper

--- tests/tests_rf/virtual_rf/helpers.py
+++ tests/tests_rf/virtual_rf/helpers.py
@@ -4,20 +4,21 @@
 """RAMSES RF - a RAMSES-II protocol decoder & analyser."""
 
 from ramses_rf import Device
-from ramses_rf.device import Fakeable as _Fakeable
+from ramses_rf.device import Fakeable
 
 
-def ensure_fakeable(dev: Device) -> None:
-    """If a Device is not Fakeable, make it so."""
+def ensure_fakeable(dev: Device, make_fake: bool = True) -> None:
+    """If a Device is not Fakeable make it so, and optional make it Faked."""
 
-    class Fakeable(dev.__class__, _Fakeable):
+    class _Fakeable(dev.__class__, Fakeable):
         pass
 
-    if isinstance(dev, _Fakeable):
-        # if hasattr(dev, "_make_fake"):  # no need for callable(getattr(...))
+    if isinstance(dev, _Fakeable | Fakeable):
         return
 
     dev.__class__ = Fakeable
-    setattr(dev, "_faked", None)
     setattr(dev, "_context", None)
-    setattr(dev, "_1fc9_state", {})
+
+    assert isinstance(dev, Fakeable)
+    if make_fake:
+        dev._make_fake()
