0561e42f9d8092c7133725e4afcf54947d16a471
David Bonnes <zxdavb@bonnes.me>
2024-02-04 00:20:35 +0000

    bump to 0.31.7

--- src/ramses_rf/version.py
+++ src/ramses_rf/version.py
@@ -1,4 +1,4 @@
 """RAMSES RF - a RAMSES-II protocol decoder & analyser (application layer)."""
 
-__version__ = "0.31.6"
+__version__ = "0.31.7"
 VERSION = __version__
--- src/ramses_tx/version.py
+++ src/ramses_tx/version.py
@@ -1,4 +1,4 @@
 """RAMSES RF - a RAMSES-II protocol decoder & analyser (transport layer)."""
 
-__version__ = "0.31.6"
+__version__ = "0.31.7"
 VERSION = __version__
