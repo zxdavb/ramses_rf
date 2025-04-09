bd191a963b92c087f6ec6b77213108c7162dbec9
Egbert Broerse <dcc2@ebroerse.nl>
2025-04-09 17:52:46 +0200

    replace ventura hack with proper pattern, remove 'speed' from rate lookup table for i18n

--- src/ramses_tx/parsers.py
+++ src/ramses_tx/parsers.py
@@ -1637,17 +1637,17 @@ def parser_22f3(payload: str, msg: Message) -> dict[str, Any]:
 
 # WIP: unknown, HVAC
 def parser_22f4(payload: str, msg: Message) -> dict[str, Any]:
-    # HACK: for dev/test: 37:153226 is ClimaRad Ventura fan/remote
-    if msg.src.id == "37:153226":
-        if payload[10:12] == "60":
-            payload = payload[8:14]
-        else:
+    # ClimaRad Ventura fan/remote
+    if msg.len == 13 and payload[14:] == "000000000000":
+        if payload[10:12] == "00":
             payload = payload[:4] + payload[12:14]
+        else:
+            payload = payload[8:14]
     else:
         payload = payload[:6]
 
     MODE_LOOKUP = {
-        0x00: "off?",
+        0x00: "off",
         0x20: "paused",
         0x40: "auto",
         0x60: "manual",
@@ -1655,12 +1655,12 @@ def parser_22f4(payload: str, msg: Message) -> dict[str, Any]:
     mode = int(payload[2:4], 16) & 0x60
     assert mode in MODE_LOOKUP, mode
 
-    RATE_LOOKUP = {
-        0x00: "speed 0",  # "off"?,
-        0x01: "speed 1",  # "low", or trickle?
-        0x02: "speed 2",  # "medium-low", or low?
-        0x03: "speed 3",  # "medium",
-        0x04: "speed 4",  # "medium-high", or high?
+    RATE_LOOKUP = {  # note no i18n, passed to UI as is
+        0x00: "0",  # "off"?,
+        0x01: "1",  # "low", or trickle?
+        0x02: "2",  # "medium-low", or low?
+        0x03: "3",  # "medium",
+        0x04: "4",  # "medium-high", or high?
         0x05: "boost",  # "boost", aka purge?
     }
     rate = int(payload[4:6], 16) & 0x03
