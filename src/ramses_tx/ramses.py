3bd5052949bda21bdce782a7f8f1ad650268b384
Egbert Broerse <dcc2@ebroerse.nl>
2025-04-17 11:15:32 +0200

    Add `22E5` log (#184)
    * add RP code to FAN, 22e5 log
    
    * annotate 22e5 log
    
    * fix Orcon spelling in note

--- src/ramses_tx/ramses.py
+++ src/ramses_tx/ramses.py
@@ -1060,6 +1060,7 @@ _DEV_KLASSES_HVAC: dict[str, dict[Code, dict[VerbT, Any]]] = {
         Code._1470: {RP: {}},
         Code._1F09: {I_: {}, RP: {}},
         Code._1FC9: {W_: {}},
+        Code._22E5: {RP: {}},
         Code._22F1: {RP: {}},
         Code._22F2: {I_: {}, RP: {}},
         Code._22F3: {},
